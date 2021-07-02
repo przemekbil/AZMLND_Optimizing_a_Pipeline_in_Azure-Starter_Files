870e3cf7289162457b8424680296c7f9fc483a6e
przemekbil <77756713+przemekbil@users.noreply.github.com>
2021-07-02 15:48:59 +0000

    Add files via upload
    Correct files selected for download. Hyperdrive parameters corrected. 
    clean_data function definition moved above the line where it's being called in train.py

--- train.py
+++ train.py
@@ -16,14 +16,6 @@ datastore_path = "https://automlsamplenotebookdata.blob.core.windows.net/automl-
 
 ds = TabularDatasetFactory.from_delimited_files(path=datastore_path)
 
-x, y = clean_data(ds)
-
-# Split data into train and test sets.
-
-x_train, x_test, y_train, y_test = train_test_split(x, y)
-
-run = Run.get_context()
-
 def clean_data(data):
     # Dict for cleaning data
     months = {"jan":1, "feb":2, "mar":3, "apr":4, "may":5, "jun":6, "jul":7, "aug":8, "sep":9, "oct":10, "nov":11, "dec":12}
@@ -51,6 +43,17 @@ def clean_data(data):
     y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
     
     return x_df, y_df
+
+
+x, y = clean_data(ds)
+
+# Split data into train and test sets.
+
+x_train, x_test, y_train, y_test = train_test_split(x, y)
+
+run = Run.get_context()
+
+
 
 def main():
     # Add arguments to script
